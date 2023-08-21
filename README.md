85387f9082ccaeb6aeb58706a0c686c5fe31ab46
Timur Bondarenko <tibode495@gmail.com>
2023-08-21 23:56:49 +0600

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # unknown-interview
+
+## Pre requierments
+
+- docker
+- docker-compose
+- node.js 20
+- pnpm
+- python 3.11
+
+Rename .env.exemple -> .env
+
+## Install and Run via docker compose
+
+```
+sudo docker-compose up
+```
---
+++ backend/app/main.py
@@ -0,0 +1,34 @@
+import asyncio
+import json
+from fastapi import FastAPI, Request
+
+from starlette.staticfiles import StaticFiles
+from starlette.responses import FileResponse
+from starlette.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import jwt
+
+templates = Jinja2Templates(directory="app/templates")
+
+
+app = FastAPI(
+    openapi_url="/api/v1/users/openapi.json", docs_url="/api/v1/users/docs", debug=True
+)
+
+@app.get("/", )
+async def hello_world():
+    return 'hello world'
+
+@app.on_event("startup")
+async def startup():
+    print("Starting up...")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    print("Shutting down...")
+
+
+@app.get("/")
+async def read_index():
+    return FileResponse("index.html")
