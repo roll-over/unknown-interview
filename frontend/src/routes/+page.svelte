bcf6dcc8f2e328a3131871cc165245af1475839a
Huge Letters <ckuka71@gmail.com>
2023-08-23 15:34:50 +0200

    querying example

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,29 @@
+<script lang="ts">
+	import { createQuery } from '@tanstack/svelte-query';
+	import type { PageData } from './$types';
+	import { _getPosts } from './+page';
+	const clientQuery = createQuery({
+		queryKey: ['cliendQuery'],
+		queryFn() {
+			return fetch('https://jsonplaceholder.typicode.com/posts/1').then((response) =>
+				response.text()
+			);
+		}
+	});
+
+	export let data: PageData;
+	let post = data.post;
+	$: hybridQuery = createQuery({
+		queryKey: ['hybridQuery', post],
+		queryFn: () => _getPosts(post)
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Visit <a href="https://kit.svelte.dev" class="bg-green-400 text-red-500">kit.svelte.dev</a> to read
+	the documentation
+</p>
+<p>{$clientQuery.data}</p>
+<p>{$hybridQuery.data}</p>
+<input bind:value={post} class="m-4 outline outline-black" placeholder="post id" />
---
+++ frontend/src/routes/+page.ts
@@ -0,0 +1,20 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ parent, fetch }) => {
+	const { queryClient } = await parent();
+
+	const post = 20;
+	await queryClient.prefetchQuery({
+		queryKey: ['hybridQuery', post],
+		queryFn: () => _getPosts(post, fetch)
+	});
+
+	return { post };
+};
+
+export function _getPosts(post: number, _fetch?: typeof fetch) {
+	if (!post) return null;
+	return (_fetch ?? fetch)(`https://jsonplaceholder.typicode.com/posts/${post}`).then((x) =>
+		x.text()
+	);
+}
