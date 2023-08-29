1609d72596107a2c09c461d920f0d0463b7e1789
dzigr <dmbrag@gmail.com>
2023-08-29 14:54:53 +0300

    Fix cv router

--- backend/app/api/routers/cv.py
+++ backend/app/api/routers/cv.py
@@ -1,3 +1,4 @@
+from typing import List
 from uuid import UUID
 from fastapi import APIRouter
 
@@ -27,7 +28,8 @@ async def create_cv(user_cv: CVRequestSchema):
 async def get_random_cv():
     random_user_cv = await CV.aggregate([{"$sample": {"size": 1}}]).to_list(1)
 
-    return random_user_cv[0]
+    if random_user_cv:
+        return random_user_cv[0]
 
 
 @cv_router.get(
@@ -35,7 +37,21 @@ async def get_random_cv():
     response_model=CVResponseSchema,
     summary="Return user CV by ID",
 )
-async def get_cv(cv_id: UUID):
+async def get_user_cv(cv_id: UUID):
     user_cv = await CV.find_one({"custom_id": cv_id})
 
     return user_cv
+
+
+@cv_router.get(
+    "/delete/{cv_id}",
+    summary="Delete user CV by ID",
+)
+async def delete_user_cv(cv_id: UUID):
+    user_cv = await CV.find_one({"custom_id": cv_id})
+    if user_cv:
+        await user_cv.delete()
+
+        return {"message": "User CV delete successfully"}
+
+    return {"error": "There is no user CV with such ID"}
