7d2e195b326f18715e598d2d4368786d6a330130
dzigr <dmbrag@gmail.com>
2023-09-07 17:27:24 +0300

    Update & refactor CV & Vacancy routers, repo

--- backend/app/repository/services/vacancy_cv.py
+++ backend/app/repository/services/vacancy_cv.py
@@ -5,8 +5,12 @@ class VacancyCVService:
     def __init__(self, repo_model: AbstractBaseRepository):
         self.repo: AbstractBaseRepository = repo_model()
 
-    async def create_one(self, data):
-        return await self.repo.create_one(data)
+    async def create_one(self, data, *, owner_data):
+        join_cv_data = self.repo.model(
+            owner_id=owner_data.custom_id,
+            **data.model_dump(),
+        )
+        return await self.repo.create_one(join_cv_data)
 
     async def get_one(self, data_id):
         return await self.repo.fetch_one({"custom_id": data_id})
