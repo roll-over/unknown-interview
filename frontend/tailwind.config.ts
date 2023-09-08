e0749b3ec70e02271eacd8bc09d31c44314294f3
VladSt <adrenaline2142@gmail.com>
2023-09-08 11:45:27 +0500

    add font family Comfortaa, 'Montserrat

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,6 +4,13 @@ import plugin from 'tailwindcss/plugin';
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		// colors: {
+    //   'blue': '#CBE6F7',
+    // },
+		fontFamily: {
+      title: ['Comfortaa'],
+			body: ['Montserrat'],
+    },
 		extend: {}
 	},
 	plugins: [
