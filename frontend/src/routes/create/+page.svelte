b5b66315cb633e52b67e80a7fb6bac798b32c130
hugeletters <ckuka71@gmail.com>
2023-09-08 16:48:57 +0200

    renamed routes patched sveltekit to include tailwind config in tsconfig file added skeleton tailwind support

--- frontend/src/routes/create/+page.svelte
+++ frontend/src/routes/create/+page.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
 </script>
 
-<h1>Get started</h1>
 <a
-	href="/hello/employees-search"
+	href="/hello/offer"
 	class="mb-2 mr-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
 	>Employess Search</a
 >
 <a
-	href="/hello/job-search"
+	href="/hello/cv"
 	class="mb-2 mr-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
 	>Job Search</a
 >
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,14 +1,20 @@
-import { Config } from 'tailwindcss';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
 import plugin from 'tailwindcss/plugin';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
 	theme: {
 		extend: {}
 	},
 	plugins: [
 		plugin(({ addVariant }) => {
 			addVariant('current', ['&:focus-within', '&:hover']);
-		})
+		}),
+		skeleton
 	]
 } satisfies Config;
