9974036a86230a03ad54f0a158b37277f2a07cfc
dzigr <dmbrag@gmail.com>
2023-09-11 14:24:04 +0300

    Update CV and Vacancy models

--- backend/app/db/models/cv.py
+++ backend/app/db/models/cv.py
@@ -1,10 +1,11 @@
-from typing import Optional
+from typing import List, Optional
 from uuid import UUID
 
 from pydantic import Field
 
 from app.db.models.mixins import Grade, Salary, Title, UUIDMixin
 from app.db.models.profession import Profession
+from app.db.models.skill import Skill
 
 
 class CV(UUIDMixin):
@@ -13,7 +14,7 @@ class CV(UUIDMixin):
     salary: Optional[Salary] = Field(None)
     grade: Grade
     profession: Profession
-    skillset: str
+    skillset: Optional[List[Skill]]
 
     class Settings:
         name = "CV"
