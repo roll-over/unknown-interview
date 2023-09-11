d60432d896effa87fdc4a823b3de2d34ad87321a
VladSt <adrenaline2142@gmail.com>
2023-09-11 09:41:17 +0500

    add custom colors

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,14 +4,19 @@ import plugin from 'tailwindcss/plugin';
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		// colors: {
-    //   'blue': '#CBE6F7',
-    // },
 		fontFamily: {
       title: ['Comfortaa'],
 			body: ['Montserrat'],
     },
-		extend: {}
+		extend: {
+			colors: {
+				'pattens-blue': '#CBE6F7',
+				'dodger-blue': '#2B93E1',
+				'alice-blue': '#EAF4FD',
+				'summer-sky': '#4DA9E9',
+				'focus-blue': '#1985E2',
+				},
+		}
 	},
 	plugins: [
 		plugin(({ addVariant }) => {
