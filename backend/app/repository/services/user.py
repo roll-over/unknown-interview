4d633c1178f14021c83258c4dff4a18285cbfb67
Grisha Malanchuk <grishamalanchuk@gmail.com>
2023-09-14 21:49:25 +0300

    refactoring

--- backend/app/repository/services/user.py
+++ backend/app/repository/services/user.py
@@ -9,14 +9,13 @@ class UserService:
     @staticmethod
     def get_user_email(func):
         def wrapper(self, data):
-            try:
-                user_email = data.get("email")
-            except AttributeError:
-                try:
-                    user_email = data.email
-                except AttributeError:
+            match data:
+                case dict():
+                    user_email = data.get("email")
+                case str():
                     user_email = data
-
+                case _:
+                    user_email = data.email
             return func(self, user_email, data)
 
         return wrapper
