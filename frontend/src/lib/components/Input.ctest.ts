efa342e4186cec818f135cdeedd6a0d3e30f5bea
hugeletters <ckuka71@gmail.com>
2023-09-16 00:34:25 +0200

    fixed typings

--- frontend/src/lib/components/Input.ctest.ts
+++ frontend/src/lib/components/Input.ctest.ts
@@ -4,7 +4,7 @@ import Input from '$lib/components/Input.svelte';
 test.use({ viewport: { width: 500, height: 500 } });
 
 test('Input', async ({ mount }) => {
-	const component = await mount(Input);
+	const component = await mount(Input, {});
 	await expect(component).toBeVisible();
 	await component.fill('123');
 	await expect(await component.inputValue()).toEqual('123');
--- frontend/src/lib/components/RadioGroup.ctest.ts
+++ frontend/src/lib/components/RadioGroup.ctest.ts
@@ -1,11 +1,11 @@
-import { test, expect } from '@playwright/experimental-ct-svelte';
 import RadioGroup from '$lib/components/RadioGroup.svelte';
+import { expect, test } from '@playwright/experimental-ct-svelte';
 
 test.use({ viewport: { width: 500, height: 500 } });
 
 test('Radio group', async ({ mount }) => {
-	const component = await mount(RadioGroup, {
-		props: { options: ['one', 'two', 'three'], name: 'profession', value: 'three' }
+	const component = await mount<RadioGroup<string>>(RadioGroup, {
+		props: { name: 'test', options: ['one', 'two', 'three'], value: 'three' }
 	});
 	await expect(component).toBeVisible();
 	await component.getByLabel('one').click();
