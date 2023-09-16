309022e882fc329e940b1581a98f9a73dd779a62
Ocean50ul <67197290+Ocean50ul@users.noreply.github.com>
2023-09-16 05:04:41 +0300

    Limit tests added, code rewritten using fixtures.

--- backend/tests/test_init.py
+++ backend/tests/test_init.py
@@ -32,37 +32,34 @@ async def test_get_note(test_client: AsyncClient):
     assert response.status_code == 200
 
 
-@pytest.mark.asyncio
-async def test_get_skill(test_client: AsyncClient):
-    params = {
-        "limit": 5
-    }
-    response = await test_client.get("/api/v1/skills/", params=params)
-    assert response.status_code == 200
+@pytest.fixture(scope="module", params=[{"name": "test_string"} for _ in range(3)])
+def json_data(request):
+    return request.param
+
+
+@pytest.fixture(scope="module")
+def expected_status():
+    return 200
+
+
+@pytest.fixture(scope="module", params=["/api/v1/skills/", "/api/v1/professions/"])
+def endpoint_urls(request):
+    return request.param
 
 
 @pytest.mark.asyncio
-async def test_add_skill(test_client: AsyncClient):
-    data = {
-        "name": "test_string"
-    }
-    response = await test_client.post("/api/v1/skills/", json=data)
-    assert response.status_code == 200
+async def test_add_skill(test_client: AsyncClient, endpoint_urls, json_data, expected_status):
+    response = await test_client.post(endpoint_urls, json=json_data)
+    assert response.status_code == expected_status
+
+
+@pytest.fixture(scope="module", params=[("limit=1", 1), ("limit=2", 2), ("", 3)])
+def expected_len(request):
+    return request.param
 
 
 @pytest.mark.asyncio
-async def test_get_profession(test_client: AsyncClient):
-    params = {
-        "limit": 5
-    }
-    response = await test_client.get("/api/v1/professions/", params=params)
-    assert response.status_code == 200
-
-
-@pytest.mark.asyncio
-async def test_add_profession(test_client: AsyncClient):
-    data = {
-        "name": "test_string"
-    }
-    response = await test_client.post("/api/v1/professions/", json=data)
-    assert response.status_code == 200
+async def test_get_sp(test_client: AsyncClient, endpoint_urls, expected_len, expected_status):
+    response = await test_client.get(endpoint_urls, params=expected_len[0])
+    assert response.status_code == expected_status
+    assert len(response.json()) == expected_len[1]
