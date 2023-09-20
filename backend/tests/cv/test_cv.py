887c58cc52964609634074bcf45ac34b79f35753
dzigr <dmbrag@gmail.com>
2023-09-20 15:55:47 +0300

    Add template tests for cv

---
+++ backend/tests/cv/test_cv.py
@@ -0,0 +1,43 @@
+import pytest
+from fastapi import status
+from httpx import AsyncClient
+
+from tests.utils import get_test_data
+
+
+class TestCV:
+    test_data = get_test_data("cv_vacancy")
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        "cv_data, expected_status",
+        [
+            (test_data.get("valid_data"), status.HTTP_401_UNAUTHORIZED),
+        ],
+    )
+    async def test_add_cv_unauthorized(
+        self, test_client: AsyncClient, test_user, cv_data, expected_status
+    ):
+        response = await test_client.post(
+            "/api/v1/cvs/",
+            json=cv_data,
+        )
+        assert response.status_code == expected_status
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        "cv_data, expected_status",
+        [
+            (test_data.get("valid_data"), status.HTTP_200_OK),
+            (test_data.get("invalid_data"), status.HTTP_422_UNPROCESSABLE_ENTITY),
+        ],
+    )
+    async def test_add_cv_authorized(
+        self, test_client: AsyncClient, test_user, cv_data, expected_status
+    ):
+        await test_user.login(test_client)
+        response = await test_client.post(
+            "/api/v1/cvs/",
+            json=cv_data,
+        )
+        assert response.status_code == expected_status
