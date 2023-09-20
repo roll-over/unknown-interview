6e0e11e9cd3448aca6f119a3717bd8c58e4e7e74
dzigr <dmbrag@gmail.com>
2023-09-20 15:46:18 +0300

    Rename modules

--- backend/tests/users/test_users.py
+++ backend/tests/users/test_users.py
@@ -11,11 +11,11 @@ async def test_post_user(test_client: AsyncClient):
     }
 
     response = await test_client.post("/api/v1/users/", json=data)
-    assert data['email'] == response.json()['email']
+    assert data["email"] == response.json()["email"]
     assert response.status_code == status.HTTP_200_OK
 
     response = await test_client.post("/api/v1/users/", json=data)
-    assert response.json() == {'detail': 'User with this email is already exists'}
+    assert response.json() == {"detail": "User with this email is already exists"}
     assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
@@ -35,18 +35,16 @@ async def test_get_user_list(test_client: AsyncClient):
 @pytest.mark.asyncio
 async def test_delete_user(test_client: AsyncClient):
     response = await test_client.delete(
-        "/api/v1/users/email@example.com",
-        params={"user_email": "email@example.com"}
+        "/api/v1/users/email@example.com", params={"user_email": "email@example.com"}
     )
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {'message': 'User deleted successfully'}
+    assert response.json() == {"message": "User deleted successfully"}
 
 
 @pytest.mark.asyncio
 async def test_delete_user_not_found(test_client: AsyncClient):
     response = await test_client.delete(
-        "/api/v1/users/not@found.com",
-        params={"user_email": "not@found.com"}
+        "/api/v1/users/not@found.com", params={"user_email": "not@found.com"}
     )
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
