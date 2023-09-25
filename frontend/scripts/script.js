af61259c8b4bf311340545835b125b5a6c3cad89
hugeletters <ckuka71@gmail.com>
2023-09-25 09:42:28 +0200

    typo

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,4 +1,4 @@
-// this file removed the need to write boilerplate when executing scripts with tsx
+// this file removes the need to write boilerplate when executing scripts with tsx
 import { spawn } from 'child_process';
 
 spawn(`pnpm tsx --tsconfig scripts/tsconfig.json scripts/index.ts ${process.argv[2] ?? ''}`, {
