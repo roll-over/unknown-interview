46ae5964cf7f6cf4e71a439c33810aae58fa9ff9
hugeletters <ckuka71@gmail.com>
2023-09-28 16:42:00 +0200

    fixed docker-dev on fresh run

--- frontend/scripts/docker-dev.ts
+++ frontend/scripts/docker-dev.ts
@@ -1,11 +1,10 @@
 import { spawn } from 'child_process';
-import { watch } from 'chokidar';
 
 export default function runDockerProcess() {
-	spawn('pnpm install', { shell: true, stdio: 'inherit' }).addListener('exit', (e) => {
+	spawn('pnpm install', { shell: true, stdio: 'inherit' }).addListener('exit', async (e) => {
 		if (e !== 0) throw Error('There was an error trying to install dependencies');
 
-		const watcher = watch('package.json');
+		const watcher = (await import('chokidar')).watch('package.json');
 		watcher.on('change', () => {
 			spawn('pnpm install', { shell: true, stdio: 'inherit' });
 		});
