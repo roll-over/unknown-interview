2ba28cb2b19739917aee40521caacdb4baf8e50a
dzigr <dmbrag@gmail.com>
2023-10-04 21:27:41 +0300

    Rename and refactor match model

--- backend/app/db/models/match.py
+++ backend/app/db/models/match.py
@@ -1,11 +1,13 @@
 from uuid import UUID
 
-from .mixins import UUIDMixin
+from .mixins import MatchRelation, UUIDMixin
 
 
-class MatchCollection(UUIDMixin):
+class Match(UUIDMixin):
     cv_id: UUID
+    applicant_relation: MatchRelation = MatchRelation.not_shown
     vacancy_id: UUID
+    employer_relation: MatchRelation = MatchRelation.not_shown
 
     class Settings:
-        name = 'matches'
+        name = "matches"
