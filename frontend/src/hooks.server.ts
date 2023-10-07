b14df31c7729e90eae5cd32b4fb5ce8341a8d57d
hugeletters <ckuka71@gmail.com>
2023-10-07 02:45:27 +0200

    pass whole cookies header

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -11,9 +11,11 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 export const handleFetch: HandleFetch = async ({ event, fetch, request }) => {
 	const apiUrl = process.env.INTERNAL_URL;
-	// todo - this needs a more robust check if fetch request we're making is to our backend to pass along our cookie
-	if (apiUrl && request.url.startsWith(apiUrl.slice(0, -3))) {
-		request.headers.set('cookie', `session=${event.cookies.get('session')}`);
+
+	const cookies = event.request.headers.get('cookie');
+	// todo - this needs a more robust check if fetch request we're making is to our backend to pass along session cookie
+	if (cookies && apiUrl && request.url.startsWith(apiUrl.slice(0, -3))) {
+		request.headers.set('cookie', cookies);
 	}
 
 	return fetch(request);
