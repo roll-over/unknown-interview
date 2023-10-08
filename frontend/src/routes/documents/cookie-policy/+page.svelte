b78aae111207afe82a25e80a1876040714ecd992
hugeletters <ckuka71@gmail.com>
2023-10-08 19:29:32 +0200

    done

--- frontend/src/routes/documents/cookie-policy/+page.svelte
+++ frontend/src/routes/documents/cookie-policy/+page.svelte
@@ -2,9 +2,10 @@
 	import { route } from '$lib/utils/route';
 </script>
 
-<a href={route("/documents")}>Back to documents</a>
+<a href={route('/documents')}>Back to documents</a>
 
 <iframe
+	title="cookie policy"
 	width="100%"
 	height="100%"
 	src="https://docs.google.com/document/d/e/2PACX-1vQCzJX06yM02m2VHTOljOsfDMcAxgxvWscuwavBbRiEbOnyEq1w8bAgJRWlGWE4AT3oEWcvepKP0StT/pub?embedded=true"
