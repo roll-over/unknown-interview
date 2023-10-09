54463d779719b7caffef948bbc2f35f590ecb106
OvchariK3208 <ovcharik320@gmail.com>
2023-10-09 12:09:55 +0300

    format

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -5,42 +5,42 @@
 
 <div class="flex min-h-screen items-center justify-center">
 	<div class="flex flex-col items-center gap-14">
-		
 		<div class="flex w-full flex-col gap-8 px-5">
 			<h1 class="text-6xl text-gray-900">Hide-Hire</h1>
 			<p class="text-2xl">
-				A project for anonymous job search - where before the first
-				call and/or within correspondence, the candidate and the employer
-				will not know anything about each other except what is indicated in the cv.
+				A project for anonymous job search - where before the first call and/or within
+				correspondence, the candidate and the employer will not know anything about each other
+				except what is indicated in the cv.
 			</p>
 		</div>
-		
-		<div class="w-full flex flex-col gap-8 px-5">
+
+		<div class="flex w-full flex-col gap-8 px-5">
 			<h2 class="text-5xl text-gray-900">Contributors</h2>
 			<p class="text-2xl">These guys made this awesome</p>
-			<ul class="w-full overflow-y-scroll flex flex-wrap">
+			<ul class="flex w-full flex-wrap overflow-y-scroll">
 				{#each contributorsList as contributor, i (contributor.id)}
-					<li class="w-1/4 flex flex-col items-center px-4">
-						<img class="w-4/5" 
+					<li class="flex w-1/4 flex-col items-center px-4">
+						<img
+							class="w-4/5"
 							src={contributor.avatar_url}
 							alt="avatar contributor"
 						/>
-						<p class="text-2xl py-2">{contributor.login}</p>
+						<p class="py-2 text-2xl">{contributor.login}</p>
 						{#if contributor.html_url}
 							<a
 								href={contributor.html_url}
-								class="text-xl py-2"
-							>github icon</a>
+								class="py-2 text-xl">github icon</a
+							>
 						{/if}
 						{#if contributor.linkedin_url}
 							<a
 								href={contributor.linkedin_url}
-								class="text-xl py-2"
-							>linkedin icon</a>
+								class="py-2 text-xl">linkedin icon</a
+							>
 						{/if}
 					</li>
 				{/each}
 			</ul>
-		</div>	
+		</div>
 	</div>
 </div>
--- frontend/src/routes/integrations/+page.svelte
+++ frontend/src/routes/integrations/+page.svelte
@@ -3,11 +3,7 @@
 
 <div class="flex min-h-screen items-center justify-center">
 	<div class="flex flex-col items-center gap-14">
-		<p class="text-[32px] font-light leading-10">
-			You can integrate hide-hire to your service
-		</p>
-		<p class="text-[40px] font-medium leading-[48px]">
-			Comming Soon
-		</p>
+		<p class="text-[32px] font-light leading-10">You can integrate hide-hire to your service</p>
+		<p class="text-[40px] font-medium leading-[48px]">Comming Soon</p>
 	</div>
 </div>
