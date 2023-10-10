1a9b229dfd326e22116d60871242fe5fbaf1ff72
hugeletters <ckuka71@gmail.com>
2023-10-10 20:57:19 +0200

    typo

--- frontend/scripts/router-gen.test.ts
+++ frontend/scripts/router-gen.test.ts
@@ -10,63 +10,63 @@ function parseType(path: string[]) {
 	return stringifyRoute(parsePath(path));
 }
 
-function construcUnion(types: string[]) {
+function constructUnion(types: string[]) {
 	return types.map((type) => '`' + type + '`');
 }
 
 describe('Test that router codegen parses', () => {
 	test('static paths', () => {
-		expect(parseType(['x', 'y', 'z'])).toEqual(construcUnion(['/x/y/z']));
-		expect(parseType([])).toEqual(construcUnion(['/']));
-		expect(parseType(['a'])).toEqual(construcUnion(['/a']));
+		expect(parseType(['x', 'y', 'z'])).toEqual(constructUnion(['/x/y/z']));
+		expect(parseType([])).toEqual(constructUnion(['/']));
+		expect(parseType(['a'])).toEqual(constructUnion(['/a']));
 	});
 
 	test('params', () => {
-		expect(parseType(['x', '[y]', 'z'])).toEqual(construcUnion([`/x/${Param}/z`]));
-		expect(parseType(['[x]'])).toEqual(construcUnion([`/${Param}`]));
-		expect(parseType(['[x]', 'y'])).toEqual(construcUnion([`/${Param}/y`]));
-		expect(parseType(['x', '[y]'])).toEqual(construcUnion([`/x/${Param}`]));
+		expect(parseType(['x', '[y]', 'z'])).toEqual(constructUnion([`/x/${Param}/z`]));
+		expect(parseType(['[x]'])).toEqual(constructUnion([`/${Param}`]));
+		expect(parseType(['[x]', 'y'])).toEqual(constructUnion([`/${Param}/y`]));
+		expect(parseType(['x', '[y]'])).toEqual(constructUnion([`/x/${Param}`]));
 	});
 
 	test('optional params', () => {
-		expect(parseType(['x', '[[y]]', 'z'])).toEqual(construcUnion([`/x/${Param}/z`, `/x/z`]));
-		expect(parseType(['[[x]]'])).toEqual(construcUnion([`/${Param}`, `/`]));
-		expect(parseType(['[[x]]', 'y'])).toEqual(construcUnion([`/${Param}/y`, `/y`]));
-		expect(parseType(['x', '[[y]]'])).toEqual(construcUnion([`/x/${Param}`, `/x`]));
+		expect(parseType(['x', '[[y]]', 'z'])).toEqual(constructUnion([`/x/${Param}/z`, `/x/z`]));
+		expect(parseType(['[[x]]'])).toEqual(constructUnion([`/${Param}`, `/`]));
+		expect(parseType(['[[x]]', 'y'])).toEqual(constructUnion([`/${Param}/y`, `/y`]));
+		expect(parseType(['x', '[[y]]'])).toEqual(constructUnion([`/x/${Param}`, `/x`]));
 	});
 
 	test('rest params', () => {
-		expect(parseType(['x', '[...y]', 'z'])).toEqual(construcUnion([`/x/${Rest}/z`, `/x/z`]));
-		expect(parseType(['[...x]'])).toEqual(construcUnion([`/${Rest}`, `/`]));
-		expect(parseType(['[...x]', 'y'])).toEqual(construcUnion([`/${Rest}/y`, `/y`]));
-		expect(parseType(['x', '[...y]'])).toEqual(construcUnion([`/x/${Rest}`, `/x`]));
+		expect(parseType(['x', '[...y]', 'z'])).toEqual(constructUnion([`/x/${Rest}/z`, `/x/z`]));
+		expect(parseType(['[...x]'])).toEqual(constructUnion([`/${Rest}`, `/`]));
+		expect(parseType(['[...x]', 'y'])).toEqual(constructUnion([`/${Rest}/y`, `/y`]));
+		expect(parseType(['x', '[...y]'])).toEqual(constructUnion([`/x/${Rest}`, `/x`]));
 	});
 
 	test('groups', () => {
-		expect(parseType(['x', '(y)', 'z'])).toEqual(construcUnion([`/x/z`]));
-		expect(parseType(['(x)'])).toEqual(construcUnion([`/`]));
-		expect(parseType(['(x)', 'y'])).toEqual(construcUnion([`/y`]));
-		expect(parseType(['x', '(y)'])).toEqual(construcUnion([`/x`]));
+		expect(parseType(['x', '(y)', 'z'])).toEqual(constructUnion([`/x/z`]));
+		expect(parseType(['(x)'])).toEqual(constructUnion([`/`]));
+		expect(parseType(['(x)', 'y'])).toEqual(constructUnion([`/y`]));
+		expect(parseType(['x', '(y)'])).toEqual(constructUnion([`/x`]));
 	});
 
 	test('multiple params', () => {
 		expect(parseType(['x', 'a-[x]-[[x]]-y', 'z'])).toEqual(
-			construcUnion([`/x/a-${Param}-${Param}-y/z`, `/x/a-${Param}--y/z`])
+			constructUnion([`/x/a-${Param}-${Param}-y/z`, `/x/a-${Param}--y/z`])
 		);
 		expect(parseType(['a-[x]-[[x]]-y'])).toEqual(
-			construcUnion([`/a-${Param}-${Param}-y`, `/a-${Param}--y`])
+			constructUnion([`/a-${Param}-${Param}-y`, `/a-${Param}--y`])
 		);
 		expect(parseType(['a-[x]-[[x]]-y', 'y'])).toEqual(
-			construcUnion([`/a-${Param}-${Param}-y/y`, `/a-${Param}--y/y`])
+			constructUnion([`/a-${Param}-${Param}-y/y`, `/a-${Param}--y/y`])
 		);
 		expect(parseType(['x', 'a-[x]-[[x]]-y'])).toEqual(
-			construcUnion([`/x/a-${Param}-${Param}-y`, `/x/a-${Param}--y`])
+			constructUnion([`/x/a-${Param}-${Param}-y`, `/x/a-${Param}--y`])
 		);
 	});
 
 	test('some complicated route', () => {
 		expect(parseType(['(x)', '[[x]]-a-[...x]', '[x]-y', '(x)', 'y', '[x]-[[x]]', 'x'])).toEqual(
-			construcUnion([
+			constructUnion([
 				`/${Param}-a-${Rest}/${Param}-y/y/${Param}-${Param}/x`,
 				`/-a-${Rest}/${Param}-y/y/${Param}-${Param}/x`,
 				`/${Param}-a-/${Param}-y/y/${Param}-${Param}/x`,
