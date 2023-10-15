2f45e12f5d496c932013c9ec3c1926280d8a28d5
hugeletters <ckuka71@gmail.com>
2023-10-15 20:32:22 +0200

    im onto something

--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -2,7 +2,7 @@ import { browser } from '$app/environment';
 import { PUBLIC_EXTERNAL_URL, PUBLIC_INTERNAL_URL, PUBLIC_IS_DOCKER } from '$env/static/public';
 import type { paths } from '$lib/openapi'; // generated from openapi-typescript
 import type { QueryFunction } from '@tanstack/svelte-query';
-import createClient from 'openapi-fetch';
+import createClient, { type FetchOptions } from 'openapi-fetch';
 
 const baseUrl = browser ? undefined : PUBLIC_IS_DOCKER ? PUBLIC_INTERNAL_URL : PUBLIC_EXTERNAL_URL;
 
@@ -32,4 +32,17 @@ async function wait(ms: number) {
 			r();
 		}, ms);
 	});
+}
+
+function produceApiCall<A extends any[], R>(fn: (...args: A) => R) {
+	return (...args: A) => {
+		return [fn(...args), otherStuff(args)] as const;
+	};
+}
+const testApiCall = produceApiCall(api.GET)('/api/v1/cvs/{cv_id}', {});
+
+api.GET('/api/v1/cvs/{cv_id}', { params: { path: { cv_id: '1' } } });
+
+function otherStuff<T>(x: T) {
+	return { whatever: x };
 }
