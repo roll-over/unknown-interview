3595f2879a619ec21af134470d37e8ef0b92107d
Ocean50ul <67197290+Ocean50ul@users.noreply.github.com>
2023-10-31 19:12:52 +0300

    unit of work deps and __init__

--- backend/app/services/unitorwork/__init__.py
+++ backend/app/services/unitorwork/__init__.py
@@ -1,9 +1,10 @@
-from .dependencies import Match_dep, UVC_dep
-from .services import MatchVacancyCVUoW, UserVacancyCVUoW, ChatMessageUoW
+from .dependencies import Chat_dep, Match_dep, UVC_dep
+from .services import ChatMessageUoW, MatchVacancyCVUoW, UserVacancyCVUoW
 
 __all__ = (
     UVC_dep,
     Match_dep,
+    Chat_dep,
     UserVacancyCVUoW,
     MatchVacancyCVUoW,
     ChatMessageUoW,
--- backend/app/services/unitorwork/dependencies.py
+++ backend/app/services/unitorwork/dependencies.py
@@ -3,7 +3,8 @@ from typing import Annotated
 from fastapi import Depends
 
 from .interfaces.base import AbstractUnitOfWork
-from .services import MatchVacancyCVUoW, UserVacancyCVUoW
+from .services import ChatMessageUoW, MatchVacancyCVUoW, UserVacancyCVUoW
 
 UVC_dep = Annotated[AbstractUnitOfWork, Depends(UserVacancyCVUoW)]
 Match_dep = Annotated[AbstractUnitOfWork, Depends(MatchVacancyCVUoW)]
+Chat_dep = Annotated[AbstractUnitOfWork, Depends(ChatMessageUoW)]
