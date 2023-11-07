82058ae7c42d732b97df98a53aecffd4bb65b07c
Timur Bondarenko <tibode495@gmail.com>
2023-11-08 01:47:18 +0600

    remake login page

--- frontend/src/routes/auth/login/+page.svelte
+++ frontend/src/routes/auth/login/+page.svelte
@@ -1,53 +1,26 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
-	import Input from '$lib/components/Input.svelte';
 </script>
 
-<div class="flex min-h-screen items-center justify-center">
-	<div class="flex flex-col gap-2 rounded-lg bg-slate-600 p-4">
-		<p class="bg-gradient-to-r from-white to-slate-400 bg-clip-text font-bold text-transparent">
-			Login with provider
-		</p>
+<div class="flex min-h-full items-center justify-center">
+	<div class="flex flex-col gap-48 rounded-lg p-4">
+		<div class="flex flex-col gap-10 items-center">
+			<h1 class="text-2xl">WELCOME!</h1>
+			<p class="bg-clip-text text-slate-800 text-transparent">
+				You need to be logged in to use the service
+			</p>
+		</div>
 		<a
-			class="w-full rounded-lg bg-white p-2 text-lg font-bold text-slate-900 transition-[letter-spacing] current:tracking-widest"
+			class="current:tracking-widest flex w-full flex-row justify-between rounded-lg border-2 bg-cyan-200 p-2 text-lg text-slate-900 transition-[letter-spacing]"
 			href="/api/v1/auth/login/google"
 		>
-			<span class="text-blue-500">G</span><span class="text-red-500">o</span><span
-				class="text-yellow-500">o</span
-			><span class="text-blue-500">g</span><span class="text-green-600">l</span><span
-				class="text-red-500">e</span
-			>
-		</a>
-		<div
-			class="h-1 w-[105%] self-center rounded-full bg-gradient-to-r from-slate-300 to-slate-500"
-		/>
-		<form
-			class="flex flex-col gap-2"
-			on:submit={(e) => {
-				new FormData(e.currentTarget).forEach((value, key) => {
-					console.log({ [key]: value });
-				});
-			}}
-		>
-			<p class="bg-gradient-to-r from-white to-slate-400 bg-clip-text font-bold text-transparent">
-				Login with email
+			Continue with:
+			<p class=" font-bold">
+				<span class="text-blue-500">G</span><span class="text-red-500">o</span><span
+					class="text-yellow-500">o</span
+				><span class="text-blue-500">g</span><span class="text-green-600">l</span><span
+					class="text-red-500">e</span
+				>
 			</p>
-			<Input
-				attrs={{
-					placeholder: 'Email',
-					type: 'email',
-					autocomplete: 'username',
-					required: true,
-					name: 'email'
-				}}
-				class="text-lg text-black"
-			/>
-			<button
-				disabled={!browser}
-				class="rounded-lg bg-slate-500 p-2 shadow-sm transition-shadow hover:shadow-lg disabled:bg-slate-300"
-			>
-				Log-in
-			</button>
-		</form>
+		</a>
 	</div>
 </div>
