04e2d61e92cceedb5c1355adde80e2c72b70a162
dzigr <dmbrag@gmail.com>
2023-11-09 19:06:22 +0300

    Update respos for chats, minor refactor mongo repo

--- backend/app/services/repository/services/chat_message.py
+++ backend/app/services/repository/services/chat_message.py
@@ -6,6 +6,8 @@ from app.services.repository.interfaces import AbstractBaseRepository
 
 
 class ChatMessageService:
+    sort_field = "+created_at"
+
     def __init__(self, repo_model: AbstractBaseRepository):
         self.repo: AbstractBaseRepository = repo_model()
 
@@ -17,14 +19,37 @@ class ChatMessageService:
             count: int,
     ) -> List[ChatMessage]:
         search_criteria = {"related_id": chat_id}
-        sort_field = "+created_at"
         offset = page * count
+
         return await self.repo.fetch_many(
             search_criteria,
-            sort_field,
+            self.sort_field,
             limit=count,
             skip=offset
         )
 
-    async def create_one(self, data: ChatMessage) -> ChatMessage:
-        return await self.repo.create_one(data=data)
+    async def get_last_message(
+            self,
+            *,
+            chat_id: UUID,
+    ) -> List[ChatMessage]:
+        search_criteria = {"related_id": chat_id}
+
+        last_message = await self.repo.fetch_many(
+            search_criteria,
+            self.sort_field,
+        )
+
+        return [last_message.pop()] if last_message else []
+
+    async def create_one(
+            self,
+            data: ChatMessage,
+            *,
+            author_id: UUID,
+    ) -> ChatMessage:
+        joined_data = self.repo.model(
+            author_id=author_id,
+            **data.model_dump(),
+        )
+        return await self.repo.create_one(joined_data)
