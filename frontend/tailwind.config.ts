b8e0278b5968559c087a3fcac02724bc538981df
VladSt <adrenaline2142@gmail.com>
2023-11-10 13:17:21 +0500

    add: hide-hire theme

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,8 +3,10 @@ import { join } from 'path';
 import type { Config } from 'tailwindcss';
 import plugin from 'tailwindcss/plugin';
 import defaultTheme from 'tailwindcss/defaultTheme';
+import { hideHireTheme } from './src/theme';
 
 export default {
+	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
 		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
@@ -22,10 +24,16 @@ export default {
 						100: '#CBE6F7',
 						400: '#4DA9E9',
 						500: '#2B93E1',
-						600: '#1985E2'
+						600: '#1985E2',
 					}
 				}
 			},
+			backgroundImage: {
+        'light-background':
+          'linear-gradient(90deg, #B8E4FF 0%, #EDF5FA 52.6%, #B8E4FF 100%)',
+				'dark-background':
+				  'linear-gradient(90deg, #151618 0%, #909498 49.32%, #151618 100%)'
+      },
 			transitionDuration: { DEFAULT: '300ms' }
 		}
 	},
@@ -36,6 +44,10 @@ export default {
 			addVariant('peer-current', [':merge(.peer):focus-within ~ &', ':merge(.peer):hover ~ &']);
 			matchVariant('not', (value) => `&:not(${value})`);
 		}),
-		skeleton
+		skeleton({
+			themes: {
+				custom: [hideHireTheme]
+			}
+		})
 	]
 } satisfies Config;
