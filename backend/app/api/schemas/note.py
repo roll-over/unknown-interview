6dd9c271ca42651c5289e978af46b9af4a12f17f
dzigr <dmbrag@gmail.com>
2023-11-24 13:39:22 +0300

    Minor refactoring of the model and schema

--- backend/app/api/schemas/note.py
+++ backend/app/api/schemas/note.py
@@ -5,26 +5,19 @@ from pydantic import BaseModel, Field
 
 
 class NoteRequestSchema(BaseModel):
-    
-    #chat_id
     related_id: UUID
-    
-    author_id: UUID
     note_text: str = Field(..., max_length=500)
 
+
 class NoteResponseSchema(BaseModel):
-    #note_id
     custom_id: UUID
-    
-    #chat_id
     related_id: UUID
-    
-    author_id: UUID
     created_at: datetime
     note_text: str
 
-class NotePatchSchema(BaseModel):
-    #note_id
-    custom_id: UUID
 
-    updated_text: str = Field(..., max_length=500)
+# class NotePatchSchema(BaseModel):
+#     updated_text: str = Field(..., max_length=500)
+
+class NotePatchSchema(BaseModel):
+    note_text: str = Field(..., max_length=500)
