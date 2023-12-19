e82d3f9e6501de50123c57b7aee2808ac6498f13
VladSt <adrenaline2142@gmail.com>
2023-12-19 21:16:05 +0500

    fix aria-current

--- frontend/src/routes/profile/chat/[id]/Chats.svelte
+++ frontend/src/routes/profile/chat/[id]/Chats.svelte
@@ -46,11 +46,11 @@
     {#each $queryChats.data as { chat_id, chat_name, last_message_text, own }, i (chat_id)}
       <li>
         <a
-          href={route((p) => `/profile/chat/${p(chat_id)}`)}
-          class="group relative block overflow-hidden rounded-lg transition after:absolute after:-right-3 after:top-0 after:h-full after:w-3 after:bg-sky-900 after:transition-transform"
-          aria-current="page"
-          bind:this={chatNodes[i]}
-        >
+        href={route((p) => `/profile/chat/${p(chat_id)}`)}
+        class="group relative block overflow-hidden rounded-lg transition after:absolute after:-right-3 after:top-0 after:h-full after:w-3 after:bg-sky-900 after:transition-transform aria-[current]:translate-x-4 aria-[current]:bg-app-blue-100 aria-[current]:after:-translate-x-3"
+        aria-current={$page.params.id === chat_id || undefined}
+        bind:this={chatNodes[i]}
+      >
           <div class="rounded-lg border-2 border-app-blue-100 p-3">
             <span class="text-xl">{chat_name}</span>
             {#if own}
