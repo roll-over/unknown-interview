42153b5476a1a1e29d84bc194b9aa5a0d3544343
VladSt <adrenaline2142@gmail.com>
2023-12-19 13:47:05 +0500

    refactoring chats and messages

--- frontend/src/routes/profile/chat/[id]/Chats.svelte
+++ frontend/src/routes/profile/chat/[id]/Chats.svelte
@@ -1,9 +1,11 @@
+
 <script lang="ts">
   import { onMount } from 'svelte';
   import { createGetQuery } from '$lib/api';
   import { createQuery } from '@tanstack/svelte-query';
   import type { Chats } from './interFace';
   import { page } from '$app/stores';
+  import { route } from '$lib/utils/route';
 
   // Создание запроса на получение чатов
   const getChats = createGetQuery('/api/v1/chats/', {
@@ -17,7 +19,6 @@
       const res = await getChats.runQuery();
       // Преобразование данных о чатах для отображения в списке
       return (res.data || []).map((chat: Chats) => {
-        // Получение последнего сообщения чата
         const lastMessage = chat.last_message?.[0];
         return {
           chat_id: chat.chat_id,
@@ -28,27 +29,37 @@
       });
     }
   });
+
+  // Массив для хранения DOM-элементов чатов
+  let chatNodes: HTMLAnchorElement[] = [];
+
+  // После монтирования компонента прокрутить к выбранному чату
+  onMount(() => {
+    const selectedChatNode = chatNodes.find((node) => node.ariaCurrent);
+    if (!selectedChatNode) return;
+    selectedChatNode.scrollIntoView({ block: 'nearest' });
+  });
 </script>
 
 <ul class="flex flex-col gap-3 overflow-y-auto pr-10">
   {#if $queryChats.data && $queryChats.data.length > 0}
-    {#each $queryChats.data as { chat_id, chat_name, last_message_text, own } (chat_id)}
+    {#each $queryChats.data as { chat_id, chat_name, last_message_text, own }, i (chat_id)}
       <li>
         <a
-          href={`/profile/chat/${chat_id}`}
+          href={route((p) => `/profile/chat/${p(chat_id)}`)}
           class="group relative block overflow-hidden rounded-lg transition after:absolute after:-right-3 after:top-0 after:h-full after:w-3 after:bg-sky-900 after:transition-transform"
-          aria-current="page">
+          aria-current="page"
+          bind:this={chatNodes[i]}
+        >
           <div class="rounded-lg border-2 border-app-blue-100 p-3">
             <span class="text-xl">{chat_name}</span>
             {#if own}
-              <!-- Отображение информации о последнем сообщении, если оно отправлено пользователем -->
               <span
                 class="block w-full overflow-x-hidden text-ellipsis whitespace-nowrap transition-[font-weight] font-normal"
               >
                 <span class="text-app-blue-600">You:</span> {last_message_text}
               </span>
             {:else}
-              <!-- Отображение информации о последнем сообщении, если оно не отправлено пользователем -->
               <span class="block w-full overflow-x-hidden text-ellipsis whitespace-nowrap transition-[font-weight] font-bold">
                 {last_message_text}
               </span>
--- frontend/src/routes/profile/chat/[id]/[chatId]/+page.svelte
+++ frontend/src/routes/profile/chat/[id]/[chatId]/+page.svelte
@@ -1,179 +1,124 @@
 <script lang="ts">
-  import { page } from '$app/stores';
-  import { createGetQuery } from '$lib/api';
-  import { createQuery, createMutation } from '@tanstack/svelte-query';
-  import { dateFormatter, type Chat, type Message, formatTimestamp } from '../interFace';
-
-  // Функция для группировки элементов массива по заданному ключу
-  function groupBy<T>(array: T[], keyFunc: (item: T) => string) {
-    return array.reduce((result: Record<string, T[]>, item) => {
-      const key = keyFunc(item);
-      result[key] = result[key] || [];
-      result[key].push(item);
-      return result;
-    }, {});
-  }
-
-  // Определение типа для чата с сообщениями
-  type ChatWithMessages = {
-    chat_name: string;
-    messages: (Message & { chat_name: string })[];
-  } | undefined;
-
-  // Инициализация переменной для текста нового сообщения
+	import { page } from '$app/stores';
+	import { createGetQuery } from '$lib/api';
+	import { createQuery, QueryObserver } from '@tanstack/svelte-query';
+	import { dateFormatter, type Chat, type Message, formatTimestamp } from '../interFace';
+	
+	// Функция для группировки массива по заданному ключу
+	function groupBy<T>(array: T[], keyFunc: (item: T) => string) {
+		return array.reduce((result: Record<string, T[]>, item) => {
+			const key = keyFunc(item);
+			result[key] = result[key] || [];
+			result[key].push(item);
+			return result;
+		}, {});
+	}
+	
+	// Запрос на получение данных о чате
+	$: messagesGet = createGetQuery<Chat>(`/api/v1/chats/${$page.params.id || ''}`);
+	
+	// Запрос на получение сообщений чата с использованием библиотеки svelte-query
+	$: queryMessage = createQuery({
+		queryKey: messagesGet.key,
+		async queryFn() {
+			try {
+				const res = await messagesGet.runQuery();
+				const data = res.data || { chat_name: '', messages: [] };
+	
+				return {
+					chat_name: data.chat_name,
+					messages: (data.messages || []).map((message: Message) => ({
+						text: message.text || null,
+						own: message.own || null,
+						created_at: message.created_at || null
+					}))
+				};
+			} catch (error) {
+				throw error;
+			}
+		}
+	});
+	
+	// Переменная для хранения текста нового сообщения
 	let newMessageText = '';
-
-// Создание мутации для добавления нового сообщения
-const addMessageMutation = createMutation<
-  string, // Тип для нового текста сообщения
-  Error,
-  Message, // Тип для данных сообщения
-  { newMessageText: string } // Тип для параметров мутации
->({
-  onMutate: async (newMessageText: string) => {
-    // Сохранение текущего состояния сообщений перед мутацией
-    const prevMessages = $queryMessage.data?.messages || [];
-
-    // Создание сообщения
-    const optimisticMessage: Message = {
-      text: newMessageText,
-      own: true,
-      created_at: new Date().toISOString(),
-      chat_name: $queryMessage.data?.chat_name || 'Default Chat Name',
-    };
-
-    // Обновление данных запроса
-    $queryMessage.data = {
-      ...$queryMessage.data!,
-      messages: [...prevMessages, optimisticMessage],
-    };
-
-    // Очистка поля ввода
-    newMessageText = '';
-
-    // Возврат предыдущего состояния для возможной отмены в случае ошибки
-    return () => {
-      $queryMessage.data = {
-        ...$queryMessage.data!,
-        messages: prevMessages,
-      };
-    };
-  },
-  onSuccess: async (data) => {
-    // Проверка структуры полученных данных
-    const receivedMessage: Message = {
-      text: (data as Message).text || null,
-      own: (data as Message).own || null,
-      created_at: (data as Message).created_at || null,
-      chat_name: $queryMessage.data?.chat_name || 'Default Chat Name',
-    };
-
-    // Обновление данных запроса после успешной мутации
-    $queryMessage.data = {
-      ...$queryMessage.data!,
-      messages: [...($queryMessage.data?.messages || []), receivedMessage],
-    };
-  },
-});
-
-// Функция для отправки сообщения
-const sendMessage = async () => {
-  // Проверка, что текст нового сообщения не пустой
-  if (newMessageText.trim() !== '') {
-    try {
-      // Выполнение мутации (отправка сообщения на сервер)
-      await addMessageMutation.mutate({ newMessageText });
-    } catch (error) {
-      // Обработка ошибки при необходимости
-      console.error('Error sending message:', error);
-    }
-  }
-};
-
-  // Создание запроса для получения чата
-  $: messagesGet = createGetQuery<Chat>(`/api/v1/chats/${$page.params.id || ''}`);
-
-  // Создание запроса с использованием Svelte Query
-  $: queryMessage = createQuery({
-    queryKey: messagesGet.key,
-    async queryFn() {
-      try {
-        // Выполнение запроса для получения данных о чате
-        const res = await messagesGet.runQuery();
-        const data = res.data || { chat_name: '', messages: [] };
-
-        // Возврат данных
-        return {
-          chat_name: data.chat_name,
-          messages: (data.messages || []).map((message: Message) => ({
-            text: message.text || null,
-            own: message.own || null,
-            created_at: message.created_at || null,
-            chat_name: data.chat_name || 'Default Chat Name',
-          })),
-        };
-      } catch (error) {
-        throw error;
-      }
-    },
-  });
-</script>
-
-{#if $queryMessage.isSuccess && $queryMessage.data}
-  {@const messageGroups = groupBy($queryMessage.data.messages, (msg) =>
-    dateFormatter(msg.created_at)
-  )}
-
-  {#each Object.entries(messageGroups) as [date, messages]}
-    {#if messages.length > 0}
-      <div class="grid h-full grid-rows-5 border-2 border-l-0 border-sky-900">
-        <div class="row-span-4 flex flex-col transition-opacity">
-          <p class="bg-app-blue-50 py-4 text-center text-xl outline outline-2 outline-sky-900">{$queryMessage.data.chat_name}</p>
-          <div class="overflow-y-hidden p-5">
-            <div class="h-full overflow-y-auto px-10 text-xl">
-              <p class="p-3 text-center">{date}</p>
-              <div class="flex flex-col gap-2">
-                {#each messages as msg (msg.created_at)}
-                  {@const isYourMessage = msg.own}
-                  <div
-                    class="max-w-lg"
-                    class:max-2xl:self-end={isYourMessage}
-                  >
-                    <div
-                      class="rounded-lg px-4 py-2 {isYourMessage
-                        ? 'bg-app-blue-50'
-                        : 'bg-app-blue-100'}"
-                    >
-                      {msg.text}
-                    </div>
-                    <p
-                      class="text-neutral-700/50"
-                      class:max-2xl:text-right={isYourMessage}
-                    >
-                      {formatTimestamp(msg.created_at)}
-                    </p>
-                  </div>
-                {/each}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    {/if}
-  {/each}
-  
-  <div class="flex gap-2 px-16 py-10 font-title outline outline-2 outline-sky-900">
-    <div class="grow rounded-3xl p-4 outline outline-1 outline-app-blue-600 focus-within:outline-2">
-      <input
-        bind:value={newMessageText}
-        placeholder="Type your message..."
-        class="flex-grow px-4 py-2 mr-4 border rounded-lg focus:outline-none"
-      />
-    </div>
-    <button on:click={sendMessage} class="px-4 py-2 text-white bg-blue-500 rounded-lg">
-      Send
-    </button>
-  </div>
-{:else}
-  <div class="row-span-full m-auto">Loading...</div>
-{/if}
+	
+	// Функция для отправки нового сообщения
+	async function sendMessage() {
+		try {
+			const response = await fetch('/api/v1/chats/add_message', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					related_id: $page.params.id,
+					text: newMessageText,
+				}),
+			});
+	
+			if (!response.ok) {
+				throw new Error('Failed to send message');
+			}
+	
+			newMessageText = '';
+		} catch (error) {
+			console.error('Error sending message:', error);
+			// Обработка ошибки по необходимости
+		}
+	}
+	</script>
+	
+	{#if $queryMessage.isSuccess && $queryMessage.data}
+		{@const messageGroups = groupBy($queryMessage.data.messages, (msg) =>
+			dateFormatter(msg.created_at)
+		)}
+	
+		<div class="grid h-full grid-rows-5 border-2 border-l-0 border-sky-900">
+			<div class="row-span-4 flex flex-col transition-opacity">
+				<p class="bg-app-blue-50 py-4 text-center text-xl outline outline-2 outline-sky-900">{$queryMessage.data.chat_name}</p>
+				<div class="overflow-y-hidden p-5">
+					<div class="h-full overflow-y-auto px-10 text-xl">
+						{#each Object.entries(messageGroups) as [date, messages] (date)}
+							{#if messages.length > 0}
+								<p class="p-3 text-center">{date}</p>
+								<div class="flex flex-col gap-2">
+									{#each messages as msg (msg.created_at)}
+										{@const isYourMessage = msg.own}
+										<div
+											class="max-w-lg {isYourMessage ? 'self-end' : ''}"
+										>
+											<div
+												class="rounded-lg px-4 py-2 {isYourMessage ? 'bg-app-blue-50' : 'bg-app-blue-100'}"
+											>
+												{msg.text}
+											</div>
+											<p
+												class="text-neutral-700/50 {isYourMessage ? 'text-right max-2xl' : ''}"
+											>
+												{formatTimestamp(msg.created_at)}
+											</p>
+										</div>
+									{/each}
+								</div>
+							{/if}
+						{/each}
+					</div>
+				</div>
+			</div>
+		</div>
+	
+		<div class="flex gap-2 px-16 py-6 font-title outline outline-2 outline-sky-900">
+			<div class="grow rounded-3xl p-4 outline outline-1 outline-app-blue-600 focus-within:outline-2">
+				<textarea
+					bind:value={newMessageText}
+					placeholder="Type your message..."
+					class="flex-grow px-4 py-2 mr-4 border rounded-lg focus:outline-none"
+				/>
+			</div>
+			<button on:click={sendMessage} class="flex items-center justify-center rounded-3xl bg-app-blue-600 px-12 text-2xl text-white">
+				SEND
+			</button>
+		</div>
+	{:else}
+		<div class="row-span-full m-auto">Loading...</div>
+	{/if}
