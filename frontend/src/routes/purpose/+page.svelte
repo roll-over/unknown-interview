016ac509e1eb1d861868d36e75033e645ac21674
Timur Bondarenko <tibode495@gmail.com>
2024-01-17 16:34:59 +0600

    fix 6

--- frontend/src/routes/purpose/+page.svelte
+++ frontend/src/routes/purpose/+page.svelte
@@ -4,32 +4,40 @@
 	import { route } from '$lib/utils/route';
 </script>
 
-<div class="flex h-full justify-center bg-light-background dark:bg-dark-background dark:text-white">
+<div class="bg-light-background dark:bg-dark-background flex h-full justify-center dark:text-white">
 	<div class="grid h-full w-full max-w-5xl grid-rows-2">
-		<h1 class="m-auto text-center font-title text-6xl">Visit Purpose</h1>
-		<div class="flex flex-wrap items-start justify-around">
+		<h1 class="font-title m-auto text-center text-6xl">Visit Purpose</h1>
+		<div class="md:gap-15 flex flex-col items-start justify-around gap-5 md:flex-row md:p-10">
 			<a
-				class="flex flex-col items-center gap-4 transition-transform current:scale-105"
+				class="current:scale-105 flex w-full flex-col items-center gap-4 transition-transform"
 				href={route('/vacancy/create')}
 			>
 				<span
-					class="rounded-xl bg-app-blue-100 px-16 py-6 text-3xl transition-colors hover:bg-sky-400 dark:bg-app-dark-blue dark:hover:bg-app-dark-light"
+					class="bg-app-blue-100 dark:bg-app-dark-blue dark:hover:bg-app-dark-light w-full rounded-xl px-16 py-6 text-center text-3xl transition-colors hover:bg-sky-400"
 					>Post your vacancy</span
 				>
 				<MaterialSymbolsLaptopMacOutline
-					class="h-40 w-40 text-app-blue-500 dark:text-app-dark-gray"
+					class="text-app-blue-500 dark:text-app-dark-gray h-40 w-40"
 				/>
 			</a>
 			<a
-				class="flex flex-col items-center gap-4 transition-transform current:scale-105"
+				class="current:scale-105 flex w-full flex-col items-center gap-4 transition-transform"
 				href={route('/cv/create')}
 			>
 				<span
-					class="rounded-xl bg-app-blue-100 px-16 py-6 text-3xl transition-colors hover:bg-sky-400 dark:bg-app-dark-blue dark:hover:bg-app-dark-light"
+					class="bg-app-blue-100 dark:bg-app-dark-blue dark:hover:bg-app-dark-light w-full rounded-xl px-16 py-6 text-center text-3xl transition-colors hover:bg-sky-400"
 					>Post your CV</span
 				>
-				<IcOutlineGroups class="h-40 w-40 text-app-blue-500 dark:text-app-dark-gray" />
+				<IcOutlineGroups class="text-app-blue-500 dark:text-app-dark-gray h-40 w-40" />
 			</a>
 		</div>
 	</div>
 </div>
+
+<style>
+	.go-btn {
+		max-width: 400px;
+		width: 100%;
+		min-width: 320px;
+	}
+</style>
