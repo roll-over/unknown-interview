b3211bdd35e2ed13bb6b22ff64dc7424310d519b
Timur Bondarenko <tibode495@gmail.com>
2024-01-17 17:03:18 +0600

    fix heights

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,20 +4,18 @@
 	import Logo from '~icons/main/logo';
 </script>
 
-<div
-	class="flex h-full flex-col items-center justify-center bg-light-background dark:bg-dark-background"
->
-	<div class="relative z-0 mb-12 flex w-full flex-col items-center font-title">
+<div class="bg-light-background dark:bg-dark-background flex flex-col items-center justify-center">
+	<div class="font-title relative z-0 mb-12 flex w-full flex-col items-center">
 		<Logo class="h-52 w-full dark:fill-white" />
-		<h1 class="text-2xl uppercase dark:text-white text-center">Skills first. Unbiased Hiring.</h1>
+		<h1 class="text-center text-2xl uppercase dark:text-white">Skills first. Unbiased Hiring.</h1>
 	</div>
 	<a
 		href={route('/purpose')}
-		class="group flex items-center justify-end gap-6 rounded-lg bg-white px-14 py-2.5 text-2xl text-black transition-colors current:bg-app-blue-400 dark:bg-app-dark-blue dark:text-white dark:hover:bg-app-dark-light"
+		class="current:bg-app-blue-400 dark:bg-app-dark-blue dark:hover:bg-app-dark-light group flex items-center justify-end gap-6 rounded-lg bg-white px-14 py-2.5 text-2xl text-black transition-colors dark:text-white"
 	>
 		go to survey!
 		<IcOutlineArrowForwardIos
-			class="text-3xl text-app-blue-500 transition-colors group-current:text-app-blue-100 dark:text-white"
+			class="text-app-blue-500 group-current:text-app-blue-100 text-3xl transition-colors dark:text-white"
 		/>
 	</a>
 </div>
