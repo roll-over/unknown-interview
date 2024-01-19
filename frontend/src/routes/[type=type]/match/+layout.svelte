d9ece713e985ddc0ada0a21acd071024006d14ec
VladSt <adrenaline2142@gmail.com>
2024-01-19 13:44:53 +0500

    fix adaptive match page

--- frontend/src/routes/[type=type]/match/+layout.svelte
+++ frontend/src/routes/[type=type]/match/+layout.svelte
@@ -1,3 +1,3 @@
-<div class="grid h-full grid-cols-[2fr,3fr] gap-x-5 px-12 py-5 text-2xl dark:text-white">
+<div class="grid h-full grid-cols-[2fr,3fr] md:gap-x-5 gap-1 md:px-12 px-1 py-1 md:py-5 md:text-2xl text-md dark:text-white">
 	<slot />
 </div>
