f419c76a8331858ca369f557054548109e1acde0
VladSt <adrenaline2142@gmail.com>
2024-01-25 15:37:15 +0500

    feat: add adaptive user-page

--- frontend/src/routes/[type=type]/match/+layout.svelte
+++ frontend/src/routes/[type=type]/match/+layout.svelte
@@ -1,5 +1,5 @@
 <div
-	class="text-md grid h-full grid-cols-[2fr,3fr] gap-1 px-1 py-1 dark:text-white md:gap-x-5 md:px-12 md:py-5 md:text-2xl"
+	class="text-md grid h-full grid-cols-1 gap-1 px-1 py-1 dark:text-white md:gap-x-5 md:px-12 md:py-5 md:text-2xl xl:grid-cols-[2fr,3fr]"
 >
 	<slot />
 </div>
